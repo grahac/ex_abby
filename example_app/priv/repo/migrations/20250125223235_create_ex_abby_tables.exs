fe9d6c033297f0eb4b036d25679553b7a7c5e423
grahac <charlie@cvgraham.com>
2025-01-25 14:37:12 -0800

    Formatting

--- example_app/priv/repo/migrations/20250125223235_create_ex_abby_tables.exs
+++ example_app/priv/repo/migrations/20250125223235_create_ex_abby_tables.exs
@@ -1,7 +1,12 @@
-defmodule ExampleApp.Repo.Migrations.CreateExAbbyTables do
+defmodule MyApp.Repo.Migrations.CreateExAbbyTables do
   use Ecto.Migration
 
-  def change do
+  def up do
+    # ExAbby.Migrations is your module that creates the tables
+    ExAbby.Migrations.create_tables()
+  end
 
+  def down do
+    ExAbby.Migrations.drop_tables()
   end
 end
